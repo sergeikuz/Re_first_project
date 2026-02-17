ef3663dd38bb15ce5234d1bd45b637cec550625f
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2026-02-17 14:20:13 +0300

    Create engine for the games

---
+++ brain_games/engine.py
@@ -0,0 +1,30 @@
+import prompt
+
+
+MAX_ROUNDS = 3
+
+
+def run_games(game):
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(
+        f'Hello, {name}!'
+        f'\n{game.RULE}'
+    )
+    item = 0
+    while item < MAX_ROUNDS:
+        question, correct_answer = game.generate_data()
+        print(f'Question: {question}')
+        answer = prompt.string('Your answer: ')
+        if correct_answer == answer:
+            print('Correct!')
+            item += 1
+        else:
+            print(
+                f"'{answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'."
+                f"\nLet's try again, {name}!"
+            )
+            break
+        if item == MAX_ROUNDS:
+            print(f'Congratulations, {name}!')
